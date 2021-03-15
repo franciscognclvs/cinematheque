9db392b57a41842027b22d65ce9ad0faf87560e7
francisco goncalves <fgp1991@gmail.com>
2021-03-14 22:15:47 -0300

    routes e index(action e view)

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,38 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: 'Movie was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def movie_params
+    params_require(:movie).permit(:title, :year, :synopsis, :director, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/movies', to: 'movies#index'
+  # get '/movies/:id', to: 'movies#show'
+  # get '/movies/new', to: 'movies#new'
+  # post '/movies', to: 'movies#create'
+  # get 'movies/:id/edit', to: 'movies#edit'
+  # patch 'movies/:id', to: 'movies#update'
+  # delete 'movies/:id', to: 'movies#destroy'
+  resources :movies
 end
